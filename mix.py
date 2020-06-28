14325956e89a3bc8d294813565d1cf27b601b98b
Thomas Marshall <tmarsha8@nd.edu>
2020-06-28 05:54:27 -0400

    Added --enable-matplotlib flag

--- mix.py
+++ mix.py
@@ -7,7 +7,6 @@ from torchvision import datasets, transforms
 from torchvision.utils import save_image
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 from image_loader import *
 from model import VAE
@@ -23,7 +22,13 @@ parser.add_argument('--model', type=str, default='model.pt',
                     help='which model to use')
 parser.add_argument('--images', nargs=2, type=str, default=(None,None),
                     help='images to mix')
+parser.add_argument('--enable-matplotlib', action='store_true', default=False,
+                    help='enables showing the mixing result with matplotlib')
 args = parser.parse_args()
+
+if args.enable_matplotlib:
+    import matplotlib.pyplot as plt
+
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
 torch.manual_seed(args.seed)
@@ -58,8 +63,10 @@ if __name__ == "__main__":
     model.eval()
 
     N_TEST_IMG = 12
-    f, a = plt.subplots(1, N_TEST_IMG+2, figsize=(32, 32))
-    plt.axis('off')
+
+    if args.enable_matplotlib:
+        f, a = plt.subplots(1, N_TEST_IMG+2, figsize=(32, 32))
+        plt.axis('off')
 
     try:
         os.mkdir('result')
@@ -70,9 +77,10 @@ if __name__ == "__main__":
         img1 = next(iter(image1_loader))[0].cuda()
         img2 = next(iter(image2_loader))[0].cuda()
 
-        a[0].imshow(np.moveaxis(img1[0].data.cpu().numpy(), 0, 2))
-        a[-1].imshow(np.moveaxis(img2[0].data.cpu().numpy(), 0, 2))
-        a[0].set_xticks(()); a[0].set_yticks(())
+        if args.enable_matplotlib:
+            a[0].imshow(np.moveaxis(img1[0].data.cpu().numpy(), 0, 2))
+            a[-1].imshow(np.moveaxis(img2[0].data.cpu().numpy(), 0, 2))
+            a[0].set_xticks(()); a[0].set_yticks(())
 
         encoded1, _ = model.encode(img1)
         encoded2, _ = model.encode(img2)
@@ -89,9 +97,11 @@ if __name__ == "__main__":
             img_copy = torch.Tensor(decoded_data.cpu())
             save_image(img_copy, 'result/{}.png'.format(i))
 
-            img = (np.moveaxis(decoded_data.data.cpu().numpy(), 0, 2))
-            a[i+1].imshow(img)
-            a[i+1].set_xticks(()); a[i+1].set_yticks(())
+            if args.enable_matplotlib:
+                img = (np.moveaxis(decoded_data.data.cpu().numpy(), 0, 2))
+                a[i+1].imshow(img)
+                a[i+1].set_xticks(()); a[i+1].set_yticks(())
 
-        plt.draw(); plt.pause(0.05)
-        plt.show()
+        if args.enable_matplotlib:
+            plt.draw(); plt.pause(0.05)
+            plt.show()
